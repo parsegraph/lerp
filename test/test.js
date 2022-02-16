0e7175370dc2ceb9241bf2a03069e027d7f44f42
Aaron Faanes <dafrito@gmail.com>
2022-02-16 17:43:56 -0600

    Add test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,23 @@
-var assert = require("assert");
-import lerp from "../dist/lerp";
+import { assert } from "chai";
+import lerp from "../src/index";
 
-describe("Package", function () {
+describe("lerp", function () {
+  it("works as documented", ()=>{
+    const start = 2;
+    const end = 4
+
+    // interp=0
+    assert.equal(lerp(start, end, 0), 2);
+
+    // interp=0.5
+    assert.equal(lerp(start, end, 0.5), 3);
+
+    // interp=1
+    assert.equal(lerp(start, end, 1), 4);
+  });
+
   it("works", ()=>{
+    assert.equal(lerp(start, end, 0.5), 3);
     assert.equal(lerp(0, 1, 1), 1);
     assert.equal(lerp(0, 1, 0), 0);
     assert.equal(lerp(0, 1, 0.5), 0.5);
