670eb6fc81c1aebc8774cf0fa845ad3839c68430
Aaron Faanes <dafrito@gmail.com>
2022-02-16 17:47:32 -0600

    Use lerp in demo

--- src/demo.ts
+++ src/demo.ts
@@ -1,11 +1,11 @@
-import todo from ".";
+import lerp from "./index";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("demo");
   root.style.position = "relative";
 
   const container = document.createElement("div");
-  container.innerHTML = `${todo()}`;
+  container.innerHTML = `${lerp(0, 10, 0.5)}`;
   container.style.position = "absolute";
   container.style.left = "0px";
   container.style.top = "0px";
