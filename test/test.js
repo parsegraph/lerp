1cda02779a4d16c68fe015c4267bb2551a5fa773
Aaron Faanes <dafrito@gmail.com>
2022-02-16 17:53:18 -0600

    Add more tests

--- test/test.js
+++ test/test.js
@@ -16,12 +16,39 @@ describe("lerp", function () {
     assert.equal(lerp(start, end, 1), 4);
   });
 
-  it("works", () => {
-    assert.equal(lerp(start, end, 0.5), 3);
+  it("works for [0,1]", () => {
     assert.equal(lerp(0, 1, 1), 1);
     assert.equal(lerp(0, 1, 0), 0);
     assert.equal(lerp(0, 1, 0.5), 0.5);
+  });
+
+  it("works for [1,2]", () => {
     assert.equal(lerp(1, 2, 0.5), 1.5);
+  });
+
+  it("works for negatives", () => {
     assert.equal(lerp(-1, -2, 0.5), -1.5);
+    assert.equal(lerp(-1, -2, 0), -1);
+    assert.equal(lerp(-1, -2, 1), -2);
+  });
+
+  it("works for overlapped endpoints like [0, 0]", () => {
+    assert.equal(lerp(0, 0, 0.5), 0);
+    assert.equal(lerp(0, 0, 0), 0);
+    assert.equal(lerp(0, 0, 1), 0);
+    assert.equal(lerp(1, 1, 0.5), 1);
+    assert.equal(lerp(1, 1, 0), 1);
+    assert.equal(lerp(1, 1, 1), 1);
+  });
+
+  it("can overload interpolation value", ()=>{
+    assert.equal(lerp(0, 1, 2), 2);
+    assert.equal(lerp(0, 1, 10), 10);
+  });
+
+  it("tolerates NaN by passing it through", ()=>{
+    assert.isNaN(lerp(NaN, 1, 2));
+    assert.isNaN(lerp(0, NaN, 10));
+    assert.isNaN(lerp(0, 0, NaN));
   });
 });
