232dd28017fbd3448ab3102864c2a5e16415dee2
Aaron Faanes <dafrito@gmail.com>
2022-02-16 17:47:09 -0600

    Use index.ts as lib name

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@
  * @param {number} v0 The initial value
  * @param {number} v1 The final value
  * @param {number} t the amount of interpolation between v0 and v1. 0 implies v0 and 1 implies v1, with 0.5 being the value halfway between v0 and v1
- * @returns {number} the interpolated value
+ * @return {number} the interpolated value
  */
 export default function lerp(v0:number, v1:number, t:number):number {
     return v0*(1-t)+v1*t
